ff2bcf6bd592c449693353b5d1bbed90aae92747
Victor Petersson <victor.petersson05123@gmail.com>
2024-10-21 09:37:10 +0200

    yippi

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/html/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(1234);
