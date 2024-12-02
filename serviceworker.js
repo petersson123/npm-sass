be72f98d588e616acec8318a3526fb7091f48a29
Lukas Andersson <lukascolinvalentin@gmail.com>
2024-12-02 12:27:09 +0100

    offline serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -6,7 +6,6 @@ const assets = [
     '/main.css',
     '/main.css.map',
     '/bootstrap.min.css',
-    '/main.scss',
     '/bilder/bakgrund.jpg',
     '/bilder/fraga1.jpg',
     '/bilder/fraga2.jpg'
@@ -14,7 +13,7 @@ const assets = [
 ];
 
 self.addEventListener('install', evt => {
-    //console.log('service worker has been installed');
+    console.log('service worker has been installed');
     evt.waitUntil(    
         caches.open(staticCacheName).then(cache => {
             console.log('caching shell assets');
@@ -23,10 +22,19 @@ self.addEventListener('install', evt => {
     );
 });
 
+
+
 self.addEventListener('activate', evt => {
-    //console.log('service woker has been activated');
+    console.log('service woker has been activated');
 });
 
+
+
 self.addEventListener('fetch', evt => {
-    //console.log('fetch event', evt);
+    console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
