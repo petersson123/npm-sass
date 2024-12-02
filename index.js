30ac769e95f523203da6d87319efb66164539224
Lukas Andersson <lukascolinvalentin@gmail.com>
2024-12-02 15:24:52 +0100

    fel

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/npm-sass/serviceworker.js')
+    navigator.serviceWorker.register('serviceworker.js')
       .then((reg) => console.log('service worker registered', reg))
       .catch((err) => console.log('service worker not registered', err));
  }
--- serviceworker.js
+++ serviceworker.js
@@ -1,37 +1,35 @@
 const staticCacheName = 'site-static';
 const assets = [
-    '/npm-sass/',
-    '/npm-sass/index.html',
-    '/npm-sass/index.js',
-    '/npm-sass/bilder/bakgrund.jpg',
-    '/npm-sass/bilder/fraga1.jpg',
-    '/npm-sass/bilder/fraga2.jpg',
-    '/npm-sass/main.css',
-    '/npm-sass/main.css.map',
-    '/npm-sass/bootstrap.min.css'
+    '/',
+    '/index.html',
+    '/index.js',    
+    '/bilder/bakgrund.jpg',
+    '/bilder/fraga1.jpg',
+    '/bilder/fraga2.jpg',
+    '/main.css',
+    '/main.css.map',
+    '/bootstrap.min.css',
+
+
 ];
 
 self.addEventListener('install', evt => {
     console.log('service worker has been installed');
-    evt.waitUntil(
+    evt.waitUntil(    
         caches.open(staticCacheName).then(cache => {
             console.log('caching shell assets');
-            return cache.addAll(assets);
+            cache.addAll(assets);
         })
     );
 });
 
+
+
 self.addEventListener('activate', evt => {
     console.log('service woker has been activated');
-    evt.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== staticCacheName)
-                    .map(key => caches.delete(key))
-            )
-        })
-    );
 });
+
+
 
 self.addEventListener('fetch', evt => {
     console.log('fetch event', evt);
