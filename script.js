e05d83686389c484df7cb0cfce787a8187541319
Lukas Andersson <lukascolinvalentin@gmail.com>
2024-12-02 10:55:03 +0100

    service worker fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const questions = [
-    { question: "Vad heter våran galax?", image: "./bilder/milky_way_galaxy_1.jpg", options: ["3", "4", "5"], answer: 1 },
-    { question: "Vad är huvudstaden i Sverige?", image: "./bilder/nedladdning1.jpg", options: ["Göteborg", "Stockholm", "Malmö"], answer: 1 },
+    { question: "Vad heter våran galax?", image: "./bilder/fraga1.jpg", options: ["3", "4", "5"], answer: 1 },
+    { question: "Vad är huvudstaden i Sverige?", image: "./bilder/fraga2.jpg", options: ["Göteborg", "Stockholm", "Malmö"], answer: 1 },
     { question: "Vilket år är det?", options: ["2023", "2024", "2025"], answer: 1 },
     { question: "Vad är färgen på himlen?", options: ["Blå", "Grön", "Röd"], answer: 0 },
     { question: "Vilken är den största planeten i vårt solsystem?", options: ["Jorden", "Mars", "Jupiter"], answer: 2 },
---
+++ serviceworker.js
@@ -0,0 +1,32 @@
+const staticCacheName = 'site-static'
+const assets = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/main.css',
+    '/main.css.map',
+    '/bootstrap.min.css',
+    '/main.scss',
+    '/bilder/bakgrund.jpg',
+    '/bilder/fraga1.jpg',
+    '/bilder/fraga2.jpg'
+
+];
+
+self.addEventListener('install', evt => {
+    //console.log('service worker has been installed');
+    evt.waitUntil(    
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('activate', evt => {
+    //console.log('service woker has been activated');
+});
+
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt);
+});
