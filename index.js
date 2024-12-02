20686fd784e57e81aa526ccf5bb57f83d64952d1
Lukas Andersson <lukascolinvalentin@gmail.com>
2024-12-02 10:03:57 +0100

    service worker

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, './')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(1234);
-
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceworker.js');
+    navigator.serviceWorker.register('serviceworker.js')
+      .then((reg) => console.log('service worker registered', reg))
+      .catch((err) => console.log('service worker not registered', err));
  }
- self.addEventListener("install", event => {
-    console.log("Service worker installed");
- });
- self.addEventListener("activate", event => {
-    console.log("Service worker activated");
- });
-
- 
